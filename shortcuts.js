7b1e9eac77bfe96098244f7bd71c20ddbacbb168
midorigoke <midorigoke@desumachi.tk>
2018-02-04 17:36:37 +0900

    add: shortcuts

---
+++ shortcuts.js
@@ -0,0 +1,21 @@
+'use strict';
+
+addEventListener('keydown', event => {
+    if (event.ctrlKey && event.keyCode >= 49 && event.keyCode <= 57){
+        for (const element of document.querySelectorAll('.js-account-item.is-selected')){
+            document.querySelector('.js-account-item.is-selected').click();
+        }
+
+        const account = document.querySelectorAll('.js-account-item')[event.keyCode - 49];
+
+        if (account !== undefined){
+            account.click();
+        }
+
+        document.querySelector('.js-show-drawer.tweet-button').click();
+    }
+    
+    if (event.keyCode === 27){
+        document.querySelector('.js-hide-drawer.tweet-button').click();
+    }
+});
